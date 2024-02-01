ad3e323d58ca6bf8abe82cf7bf757ffb3ea436b5
Хорев Павел Александрович <khorev05@list.ru>
2024-02-01 10:00:20 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -19,9 +19,27 @@ function displayMovies(data) {
         const movieEl = document.createElement("div");
         movieEl.classList.add("movie");
         movieEl.innerHTML = `
-        <div class="movie_cover-inner">
+        <div class="movie__cover-inner">
                     <img src="${movie.img}"
-                        class="movie_cover" />
+                    class="movie__cover" 
+                    />
+                    <div class="movie__category">
+                        <div class="name__category"></div>
+                        <div class="details__category">${movie.releaseDate}</div>
+                    </div>
+                </div>
+                <div class="movie__info">
+                    <div class="movie__title">${movie.name}</div>
+                    <div class="movie__subtitle">${movie.originalName}</div>
+                    <div class="rating" data-total-value="4">
+                        <div class="rating__item" data-item-value="5">★</div>
+                        <div class="rating__item" data-item-value="4">★</div>
+                        <div class="rating__item" data-item-value="3">★</div>
+                        <div class="rating__item" data-item-value="2">★</div>
+                        <div class="rating__item" data-item-value="1">★</div>
+                        </div>
+                    <div class="movie__cinemaSerch">kinopoisk - ${movie.userRatings.kinopoisk}</div>
+                    <a><button class="button__button">Подробнее</button></a>
                 </div>
         `;
         moviesEl.appendChild(movieEl);
